b355fe6cf9f261e66e320ddb0192b8fbc64fc116
NOOBDY <htommyf12@gmail.com>
2020-12-07 23:12:14 +0800

    added check.go haven't wrote anything yet

--- main.go
+++ main.go
@@ -7,9 +7,19 @@ import (
 
 func main() {
 	board := tools.InitBoard()
+	hasEnded := false
 
 	for round := 1; round <= 9; round++ {
 		board = tools.Input(board, round)
+		hasEnded = tools.Check(board)
+
+		if hasEnded {
+			fmt.Printf("Player %d has won", (round+1)%2+1)
+			break
+		}
+	}
+	if !hasEnded {
+		fmt.Printf("Tie")
 	}
 
 	fmt.Scanln()
---
+++ tools/check.go
@@ -0,0 +1,8 @@
+package tools
+
+// Check checks if any of the players have won yet
+func Check(board Board) bool {
+	var hasEnded bool
+
+	return hasEnded
+}
