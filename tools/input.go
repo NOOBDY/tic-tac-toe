4bb7362166b63c63d35e0667b2160dc5a05acf13
NOOBDY <htommyf12@gmail.com>
2021-03-01 23:09:48 +0800

    bug fix: empty input now does not crash the program

--- tools/input.go
+++ tools/input.go
@@ -1,7 +1,9 @@
 package tools
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -12,14 +14,16 @@ func Input() (int, int) {
 		var input string
 
 		fmt.Print("Enter Position: ")
-		fmt.Scanln(&input)
-
-		if strings.Contains("ABC", strings.ToUpper(string(input[0]))) && strings.Contains("123", string(input[1])) {
+		scanner := bufio.NewScanner(os.Stdin)
+		if scanner.Scan() {
+			input = scanner.Text()
+		}
+		if input == "" || !(strings.Contains("ABC", strings.ToUpper(string(input[0]))) && strings.Contains("123", string(input[1]))) {
+			fmt.Println("Invalid Input")
+		} else {
 			x = int(input[0]) - 97
 			y = int(input[1]) - 49
 			break
-		} else {
-			fmt.Println("Invalid Input")
 		}
 	}
 	return x, y
